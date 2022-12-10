bfd934a578b05971536a2677f811d32597faebc4
riwajp <riwajfictious@gmail.com>
2022-12-10 14:00:41 +0545

    express server

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const port = 5000;
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const cors = require("cors");
 
-//====================================================================================
-
-const io = require("socket.io")(port, {
-  cors: {
-    origin: ["http://localhost:3000"],
-  },
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Hello world",
+  });
 });
 
+const io = require("socket.io")(http, {
+  cors: {
+    origin: ["https://riwaj-tetris.netlify.app", "http://localhost:3000"],
+  },
+});
 io.on("connection", (socket) => {
   console.log(socket.id);
 
@@ -16,5 +23,10 @@ io.on("connection", (socket) => {
     socket.broadcast.emit("recieve-state", name, matrix, score);
   });
 });
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+//====================================================================================
 
 //====================================================================================
