9c3e169a464960ed6339fbf474bbdbb2d9741cce
riwajp <riwajfictious@gmail.com>
2022-12-10 13:37:11 +0545

    added http server

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const port = 5000;
-
-//====================================================================================
-
-const io = require("socket.io")(port, {
+const httpServer = require("http").createServer();
+const io = require("socket.io")(httpServer, {
   cors: {
     origin: ["https://riwaj-tetris.netlify.app", "http://localhost:3000"],
   },
+  // ...
 });
+//====================================================================================
 
 io.on("connection", (socket) => {
   console.log(socket.id);
@@ -16,5 +16,6 @@ io.on("connection", (socket) => {
     socket.broadcast.emit("recieve-state", name, matrix, score);
   });
 });
+httpServer.listen(port);
 
 //====================================================================================
